b8e01b232883f1829ef83a185fa07bffe9c9e0ee
henriquel95 <henriquebraayann@gmail.com>
2023-06-06 20:16:45 -0300

    Entrada de idade pelo usuario

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -1,9 +1,28 @@
+import java.rmi.server.ServerNotActiveException;
+import java.util.Scanner;
+
 /**
  * LivrariaApplication
  */
 public class LivrariaApplication {
 
 public static void main(String[] args) {
-    System.out.println("Olá, mundo!");
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o seu nome: ");
+String nome = sc.nextLine();
+
+System.out.println("Digite sua idade :");
+int idade = sc.nextInt();
+if (idade < 18){
+System.out.println("Voce é menor de idade!");
+
+}else{System.out.println("Voce é maior de idade!");}
+
+
+
+
+sc.close();
+System.out.println("Olá," + nome + "!");
 }
 }
